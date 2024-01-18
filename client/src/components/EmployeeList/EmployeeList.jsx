66f9147054e491bd2b58c68e9b7117335a80c47a
Gyulo94 <qkrcjfrb112@gmail.com>
2024-01-18 16:49:32 +0900

    리액트 axios를 이용하여  직원리스트 화면 구현

---
+++ client/src/components/EmployeeList/EmployeeList.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import styles from "./EmployeeList.module.css";
+import { listEmployee } from "../../service/EmployeeService";
+
+export default function EmployeeList() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    listEmployee()
+      .then((res) => setEmployees(res.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>직원 목록</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>직원 ID</th>
+            <th>직원 이름</th>
+            <th>직원 직급</th>
+            <th>직원 이메일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((emp) => (
+            <tr key={emp.id}>
+              <th>{emp.id}</th>
+              <th>{emp.name}</th>
+              <th>{emp.jobGrade}</th>
+              <th>{emp.email}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
