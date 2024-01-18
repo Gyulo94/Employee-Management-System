a3fd3327ac20e8a1a984fbc92b571722640c93d5
Gyulo94 <qkrcjfrb112@gmail.com>
2024-01-18 14:55:27 +0900

    파일 디렉토리 변경, 리액트 프로젝트 추가

---
+++ server/src/main/java/com/employee/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.employee.service;
+
+import java.util.List;
+
+import com.employee.dto.EmployeeDTO;
+
+public interface EmployeeService {
+    EmployeeDTO createEmployee(EmployeeDTO employeeDTO);
+
+    EmployeeDTO getEmployeeById(Long employeeId);
+
+    List<EmployeeDTO> getAllEmployees();
+
+    EmployeeDTO updateEmployee(Long employeeId, EmployeeDTO updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
